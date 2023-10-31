0d9f87a113d980fd4c90219417992c0f7f4a0a5f
ekazantsev <ekazantsev@ocs.ru>
2023-11-01 01:11:28 +0400

    изменена группа

--- src/main/resources/projectFiles/build.gradle.kts
+++ src/main/resources/projectFiles/build.gradle.kts
@@ -43,6 +43,6 @@ repositories {
 }
 
 dependencies {
-    implementation("ru.kazantsev.nsd.sdk:global_variables:1.0.0")
+    api("ru.kazantsev.nsd.sdk:global_variables:1.0.0")
     implementation("org.jetbrains:annotations:16.0.1")
 }
